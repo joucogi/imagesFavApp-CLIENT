424fbff37a1ea2087444576407eece2461655a60
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2017-11-13 11:32:10 +0100

    files reorganization

--- 01-create-angular-app-with-webpack/webpack.config.js
+++ 01-create-angular-app-with-webpack/webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require("webpack");
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/app/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js"
